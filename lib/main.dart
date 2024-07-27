8f0d0afaad8233952248bdb982218514a2bb3f91
Bassem Tarek <ibassemtarek@gmail.com>
2024-07-27 15:33:28 +0300

    update home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'src/home_screen2.dart';
+import 'package:session_flutter/src/home_screen.dart';
 
 void main() {
-  runApp(const MyWidget2());
+  runApp(const HomePage());
 }
